bdd3277a4aa00cb360b9a5c071e1bd9cca1b4afe
oliver <olivercastroteran@gmail.com>
2021-01-22 19:41:16 -0400

    backend pdf-generation functionality implemented

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+const pdf = require('html-pdf');
+const cors = require('cors');
+
+const pdfTemplate = require('./documents');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParse.json());
+
+// POST - PDF generation and data fetching
+app.post('/create-pdf', (req, res) => {
+  pdf.create(pdfTemplate(req.data), {}).toFile('result.pdf', (err) => {
+    if (err) {
+      return Promise.reject();
+    }
+    Promise.resolve();
+  });
+});
+
+// GET - Send generated PDF to the client
+app.get('/fetch-pdf', (req, res) => {
+  res.sendFile(`${__dirname}/result.pdf`);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
