b17185bba2b0aaf2a39489a61b6c0aeef03a3ff2
oliver <olivercastroteran@gmail.com>
2021-01-22 15:25:33 -0400

    frontend form builded

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({
+    name: '',
+    receiptId: '',
+    price1: 0,
+    price2: 0,
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+
+  const createAndDownloadPdf = () => {
+    axios.post('/create-pdf', data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Receipt ID"
+        name="receiptId"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Price 1"
+        name="price1"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Price 2"
+        name="price2"
+        onChange={handleChange}
+      />
+
+      <button onClick={createAndDownloadPdf}>Download PDF</button>
     </div>
   );
 }
